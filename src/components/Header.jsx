bd62490997488c3dbbe5219c24b156af69f12aec
Fruit de la passion <passionfruit@passionfruit.me>
2017-10-16 14:21:11 +0200

    react-skeleton done as shown on the course

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Header = React.createClass({
+  render: function() {
+    retrun (
+      <div class="header">
+        <h1>Having fun with Raect</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = Header;
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var List = require('./components/List.jsx');
+var Header = require('./components/Header.jsx');
+
+ReactDOM.render(<Header />, document.getElementById('ingredients'));
